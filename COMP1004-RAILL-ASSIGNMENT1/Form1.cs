b03727572b54c918ef8394d229ac06e372890a4c
Tiffany Raill <tiffany.raill@mygeorgian.ca>
2017-01-16 16:32:58 -0500

    Initial Commit, Basic Form Layout, No functions

--- COMP1004-RAILL-ASSIGNMENT1/Form1.cs
+++ COMP1004-RAILL-ASSIGNMENT1/Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace COMP1004_RAILL_ASSIGNMENT1
 {
-    public partial class Form1 : Form
+    public partial class SalesBonus : Form
     {
-        public Form1()
+        public SalesBonus()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- COMP1004-RAILL-ASSIGNMENT1/Program.cs
+++ COMP1004-RAILL-ASSIGNMENT1/Program.cs
@@ -16,7 +16,7 @@ namespace COMP1004_RAILL_ASSIGNMENT1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SalesBonus());
         }
     }
 }
